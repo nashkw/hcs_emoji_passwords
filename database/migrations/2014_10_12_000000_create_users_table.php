18ca1d0d2b1e3744ffe6b619ad934fe60def0b38
Nash Waugh <2517003w@student.gla.ac.uk>
2024-01-31 15:29:34 +0000

    utf8mb4 charset
    changed the charset to utf8mb4 to allow for storage of emoji characters in the database
    
    note that this also required the local mysql database to be altered

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,6 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
